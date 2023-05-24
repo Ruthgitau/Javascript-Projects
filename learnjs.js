ed051736eeeb5d38e4de3344d86690141b9da737
Ruthgitau <gitaur72@gmail.com>
2023-05-24 11:07:57 +0300

    functions

---
+++ learnjs.js
@@ -0,0 +1,14 @@
+//function that calculates the area of a rectangle
+function area(x,y){
+document.writeln("the area of the rectangle is  "+(x*y)+"<br>")
+}
+area(2,5)
+area(6,9)
+
+//function to calculate the area of a circle
+const a=3.142
+function area_c(a,r){
+    document.writeln("the area of the circle is  "+(a*r*r)+"<br>")
+}
+area_c(a,6)
+area_c(a,7)
